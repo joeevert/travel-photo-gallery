9290d0c20f36af30e7b6ed93bd231ec661d239eb
Joe Evert <joeevert@gmail.com>
2018-10-28 11:15:48 -0500

    added pics, added picCard class to galleryItems

--- server/modules/gallery.data.js
+++ server/modules/gallery.data.js
@@ -2,7 +2,9 @@ const galleryItems = [
     { id: 1, path: 'images/cambodia.jpg', description: 'Photo of a Cambodia.', likes: 0 },
     { id: 2, path: 'images/croatia.jpg', description: 'Photo of Croatia.', likes: 0 },
     { id: 3, path: 'images/peru.jpg', description: 'Photo of Peru.', likes: 0 },
-    { id: 4, path: 'images/italy.jpg', description: 'Photo of Italy.', likes: 0 }
+    { id: 4, path: 'images/italy.jpg', description: 'Photo of Italy.', likes: 0 },
+    { id: 5, path: 'images/myanmar.jpg', description: 'Photo of Myanmar.', likes: 0 },
+    { id: 6, path: 'images/utah.jpg', description: 'Photo of Utah.', likes: 0 }
 ];
 
 module.exports = galleryItems;
