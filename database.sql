a75139a5f067394b31cc97a769114e29033f2078
Joe Evert <joeevert@gmail.com>
2018-10-29 09:12:33 -0500

    updated database.sql to match Postico

--- database.sql
+++ database.sql
@@ -3,26 +3,26 @@ CREATE TABLE galleryitems (
     id SERIAL PRIMARY KEY,
 	path VARCHAR (250),
 	description VARCHAR (250),
-    likes INTEGER
+    likes INTEGER DEFAULT 0
 );
 
 -- test items
-INSERT INTO galleryitems (path, description, likes)
-VALUES ('images/cambodia.jpg', 'Photo of a Cambodia.', 0);
+INSERT INTO galleryitems (path, description)
+VALUES ('images/cambodia.jpg', 'Photo of a Cambodia.');
 
-INSERT INTO galleryitems (path, description, likes)
-VALUES ('images/croatia.jpg', 'Photo of a Croatia.', 0);
+INSERT INTO galleryitems (path, description)
+VALUES ('images/croatia.jpg', 'Photo of a Croatia.');
 
-INSERT INTO galleryitems (path, description, likes)
-VALUES ('images/peru.jpg', 'Photo of a Peru.', 0);
+INSERT INTO galleryitems (path, description)
+VALUES ('images/peru.jpg', 'Photo of a Peru.');
 
-INSERT INTO galleryitems (path, description, likes)
-VALUES ('images/italy.jpg', 'Photo of a Italy.', 0);
+INSERT INTO galleryitems (path, description)
+VALUES ('images/italy.jpg', 'Photo of a Italy.');
 
-INSERT INTO galleryitems (path, description, likes)
-VALUES ('images/myanmar.jpg', 'Photo of a Myanmar.', 0);
+INSERT INTO galleryitems (path, description)
+VALUES ('images/myanmar.jpg', 'Photo of a Myanmar.');
 
-INSERT INTO galleryitems (path, description, likes)
-VALUES ('images/utah.jpg', 'Photo of a Utah.', 0);
+INSERT INTO galleryitems (path, description)
+VALUES ('images/utah.jpg', 'Photo of a Utah.');
 
 UPDATE galleryitems SET likes=likes+1 WHERE id=1;
